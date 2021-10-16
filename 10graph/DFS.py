c8acfd4ca871a1e0708ba5b689b2a4729ea2c5aa
PanghoChao <87929190+PanghoChao@users.noreply.github.com>
2021-10-16 17:33:48 +0900

    Update DFS.py

--- 10graph/DFS.py
+++ 10graph/DFS.py
@@ -2,6 +2,7 @@
 # 인접리스트를 
 # 딕셔너리를 입력값으로 해서 DFS를 구현 해보자
 
+
 graph ={
         1:[2,3,4], 
         2:[5], 
@@ -11,6 +12,9 @@ graph ={
         6:[], 
         7:[3]
     }
+
+#재귀를 이용한 DFS
+
 def recursive_dfs(v, discovered=[]):
     discovered.append(v)
     for w in graph[v]:
@@ -19,3 +23,23 @@ def recursive_dfs(v, discovered=[]):
     return discovered
 
 print(recursive_dfs(1))
+
+
+#스택을 이용한 DFS
+
+def iterative_dfs(start_v):
+    discovered= []
+    stack = [start_v]
+    while stack:
+        v = stack.pop()
+        if v not in discovered:
+            discovered.append(v)
+            for w in graph[v]:
+                stack.append(w)
+    return discovered
+
+print(iterative_dfs(1))
+
+# little difference code result 
+# but both first code and second code is DFS
+
